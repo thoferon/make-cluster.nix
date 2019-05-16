2730730226917287bc44695f97701b59f9356ddd
Thomas Feron <thomas.feron@redspline.com>
2019-05-16 10:25:52 +0100

    cleanup: remove unused files and add default.nix

---
+++ default.nix
@@ -0,0 +1,1 @@
+import ./lib/make-cluster.nix
--- sources.nix
+++ sources.nix
@@ -5,16 +5,4 @@ rec {
     rev = "d40823f6efe6af7bd05d25bf29a88c80b51081e1";
     ref = "wireguard-publickeyfile";
   };
-
-  hydrogen = {
-    inherit nixpkgs;
-  };
-
-  helium = {
-    inherit nixpkgs;
-  };
-
-  lithium = {
-    inherit nixpkgs;
-  };
 }
